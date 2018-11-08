d9eef2fab0a47eb7d932937b368a4ca7af55f30a
Md-Hiccup <ask2md@gmail.com>
2018-11-08 15:41:15 +0530

    6-(cont.) translation, rotation, resize done

--- 6-Image_Processing/translation.py
+++ 6-Image_Processing/translation.py
@@ -22,7 +22,7 @@ cv2.imshow("Shifted Down and Right", shifted)
 M = np.float32([[1, 0, -50], [0, 1, -90]])      
 shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
 cv2.imshow("Shifted Up and Left", shifted)
-cv2.waitKey(0)
+# cv2.waitKey(0)
 
 shifted = imutils.translate(image, 0, 100)
 cv2.imshow("shifted Down", shifted)
--- lib/imutils.py
+++ lib/imutils.py
@@ -11,3 +11,17 @@ def translate(image, x, y):
     M = np.float32([[1, 0, x], [0, 1, y]])
     shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
     return shifted
+
+def rotate(image, angle, center=None, scale=1.0):
+    (h, w) = image.shape[:2]
+    if center is None:
+        center = (w // 2, h // 2)
+
+    """
+        scale 1.0 = same size of an image.
+        scale 2.0 = double the size of an image.
+        scale 0.5 = halves the size of an image.
+    """
+    M = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated = cv2.warpAffine(image, M, (w, h))
+    return rotated
